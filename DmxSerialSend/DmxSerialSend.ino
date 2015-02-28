09f71237e32fac680599e26192bfa4b22acd00fe
Jan Van Sweevelt <vansweej@gmail.com>
2015-02-28 21:20:12 +0100

    Initial project setup

---
+++ DmxSerialSend/DmxSerialSend.ino
@@ -0,0 +1,40 @@
+// - - - - -
+// DmxSerial - A hardware supported interface to DMX.
+// DmxSerialSend.ino: Sample DMX application for sending 3 DMX values:
+// address 1 (red) -> also available on PWM Port 9
+// address 2 (green) -> also available on PWM Port 6
+// address 3 (blue) -> also available on PWM Port 5
+// 
+// Copyright (c) 2011 by Matthias Hertel, http://www.mathertel.de
+// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// 
+// Documentation and samples are available at http://www.mathertel.de/Arduino
+// 25.07.2011 creation of the DmxSerial library.
+// 10.09.2011 fully control the serial hardware register
+//            without using the Arduino Serial (HardwareSerial) class to avoid ISR implementation conflicts.
+// 01.12.2011 include file and extension changed to work with the Arduino 1.0 environment
+// - - - - -
+
+#include "DMXSerial.h"
+
+uint8_t dmxValue = 0;
+
+void setup() {
+  DMXSerial.maxChannel(512);
+
+  DMXSerial.init(DMXController);
+} // setup
+
+
+// loop through the rainbow colors 
+void loop() {
+  dmxValue++;
+  
+  DMXSerial.write(1, 100);
+  
+  int sensorValue = analogRead(A0);
+  
+  DMXSerial.write(2, sensorValue >> 2);
+
+  
+} // loop

7e9ab301396d3e81aa791ac841c6ca986feb2076
Steven Hardy <j4xxx.hardy@gmail.com>
2015-03-02 00:05:27 +0100

    update 2 encoders, full joystick

--- DmxSerialSend/DmxSerialSend.ino
+++ DmxSerialSend/DmxSerialSend.ino
@@ -18,19 +18,30 @@
 #include "DMXSerial.h"
 // #include "encoder.h"
 
- int val = 4; 
+ int val = 4;
+ int val1 = 7; 
  int encoder0PinA = 2;
  int encoder0PinB = 3;
+ int encoder1PinA = 5;
+ int encoder1PinB = 6;
  uint8_t encoder0Pos = 0;
  int encoder0PinALast = LOW;
- int n = LOW;
- int i = HIGH;
+ uint8_t encoder1Pos = 0;
+ int encoder1PinALast = LOW;
+ int n,m,o,p = LOW;
+ int i,j,k,l = HIGH;
+ int lastsensorValue = 0;
+ int lastsensorValue2 = 0;
 
 void setup() {
 
    pinMode (encoder0PinA,INPUT_PULLUP);
    pinMode (encoder0PinB,INPUT_PULLUP);
    pinMode (val, INPUT_PULLUP);
+   
+   pinMode (encoder1PinA,INPUT_PULLUP);
+   pinMode (encoder1PinB,INPUT_PULLUP);
+   pinMode (val1, INPUT_PULLUP);
 
   DMXSerial.maxChannel(512);
 
@@ -51,14 +62,39 @@ void loop() {
      
    } 
    encoder0PinALast = n;
-  
+   
    i = digitalRead(val);
    if (i == LOW) {
     encoder0Pos = 95;
    }
+   
+   m = digitalRead(encoder1PinA);
+   if ((encoder1PinALast == LOW) && (m == HIGH)) {
+     if (digitalRead(encoder1PinB) == LOW) {
+       encoder1Pos--;
+     } else {
+       encoder1Pos++;
+     }
+     
+   } 
+   encoder0PinALast = m;
+   
+   j = digitalRead(val1);
+   if (j == LOW) {
+    encoder1Pos = 95;
+   }
   
   int sensorValue1 = analogRead(A0);
+  if (sensorValue1 != lastsensorValue) {
   DMXSerial.write(2, sensorValue1 >> 2);
+  }
+  lastsensorValue = sensorValue1;
+  
+  int sensorValue2 = analogRead(A1);
+  if (sensorValue2 != lastsensorValue2) {
+  DMXSerial.write(2, sensorValue2 >> 2);
+  }
+  lastsensorValue2 = sensorValue2;
   
   // int sensorValue2 = analogRead(A1);
   DMXSerial.write(1, encoder0Pos);

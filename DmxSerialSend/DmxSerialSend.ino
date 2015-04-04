d005e8c49ab625e5c566ff732da7626639ac2958
Steven Hardy <j4xxx.hardy@gmail.com>
2015-04-04 20:02:35 +0200

    Updat encoders

--- DmxSerialSend/DmxSerialSend.ino
+++ DmxSerialSend/DmxSerialSend.ino
@@ -1,43 +1,30 @@
-// - - - - -
-// DmxSerial - A hardware supported interface to DMX.
-// DmxSerialSend.ino: Sample DMX application for sending 3 DMX values:
-// address 1 (red) -> also available on PWM Port 9
-// address 2 (green) -> also available on PWM Port 6
-// address 3 (blue) -> also available on PWM Port 5
-// 
-// Copyright (c) 2011 by Matthias Hertel, http://www.mathertel.de
-// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
-// 
-// Documentation and samples are available at http://www.mathertel.de/Arduino
-// 25.07.2011 creation of the DmxSerial library.
-// 10.09.2011 fully control the serial hardware register
-//            without using the Arduino Serial (HardwareSerial) class to avoid ISR implementation conflicts.
-// 01.12.2011 include file and extension changed to work with the Arduino 1.0 environment
-// - - - - -
-
 #include "DMXSerial.h"
 
- int bank;
  int val=4;
  int val0 = 7;
  int val1 = 10; 
+ int val2 = 13;
  int encoder0PinA = 2;
  int encoder0PinB = 3;
  int encoder1PinA = 5;
  int encoder1PinB = 6;
  int encoder2PinA = 8;
  int encoder2PinB = 9;
+ int encoder3PinA = 11;
+ int encoder3PinB = 12;
  uint8_t encoder0Pos = 0;
  int encoder0PinALast = LOW;
  uint8_t encoder1Pos = 0;
  int encoder1PinALast = LOW;
  uint8_t encoder2Pos = 0;
  int encoder2PinALast = LOW;
- int n,m,o,p = LOW;
+ uint8_t encoder3Pos = 0;
+ int encoder3PinALast = LOW;
+ int n,m,o,p,q = LOW;
  int i,j,k,l = HIGH;
  int lastsensorValue = 0;
  int lastsensorValue2 = 0;
- int a,b,c,d,x,y;
+ int a,b,c,d,e,f;
  
  
 void setup() {
@@ -49,24 +36,23 @@ void setup() {
    pinMode (encoder1PinA,INPUT_PULLUP);
    pinMode (encoder1PinB,INPUT_PULLUP);
    pinMode (val0, INPUT_PULLUP);
+   
+   pinMode (encoder2PinA,INPUT_PULLUP);
+   pinMode (encoder2PinB,INPUT_PULLUP);
+   pinMode (val1, INPUT_PULLUP);
+   
+   pinMode (encoder3PinA,INPUT_PULLUP);
+   pinMode (encoder3PinB,INPUT_PULLUP);
+   pinMode (val2, INPUT_PULLUP);
 
   DMXSerial.maxChannel(512);
 
   DMXSerial.init(DMXController);
 } // setup
 
-void dmxchannel(){
-  i = bank+1;
-  x = i+(bank*5);
-  y = (i+1)+(bank*5);
-  a = (i+2)+(bank*5);
-  b = (i+3)+(bank*5);
-  c = (i+4)+(bank*5);
-}
-
 
 void loop() {
-  
+   
    
   n = digitalRead(encoder0PinA);  // encoder 1
    if ((encoder0PinALast == LOW) && (n == HIGH)) {
@@ -75,17 +61,14 @@ void loop() {
      } else {
        encoder0Pos++;
      }
-       
+    
+    
    encoder0PinALast = n;  
     encoder0Pos = 0;
-    
-    if(n<0, n==4);
-    if(n>4, n==0);
    
    n = digitalRead(val); 
     }
   
-  bank = n;
   
    m = digitalRead(encoder1PinA);   //encoder 2
    if ((encoder1PinALast == LOW) && (m == HIGH)) {
@@ -98,13 +81,13 @@ void loop() {
    } 
    encoder1PinALast = m;
    
-   j = digitalRead(val0);
-   if (j == LOW) {
-    encoder1Pos = 127;
+   m = digitalRead(val0);
+   if (m == LOW) {
+    encoder1Pos = 0;
    }
    
    o = digitalRead(encoder2PinA);   //encoder 2
-   if ((encoder2PinALast == LOW) && (m == HIGH)) {
+   if ((encoder2PinALast == LOW) && (o == HIGH)) {
      if (digitalRead(encoder1PinB) == LOW) {
        encoder2Pos--;
      } else {
@@ -114,9 +97,26 @@ void loop() {
    } 
    encoder2PinALast = o;
    
-   k = digitalRead(val1);
-   if (k == LOW) {
-    encoder2Pos = 127;
+   o = digitalRead(val1);
+   if (o == LOW) {
+    encoder2Pos = 0;
+   }
+   
+   
+   p = digitalRead(encoder3PinA);   //encoder 2
+   if ((encoder3PinALast == LOW) && (p == HIGH)) {
+     if (digitalRead(encoder3PinB) == LOW) {
+       encoder3Pos--;
+     } else {
+       encoder3Pos++;
+     }
+     
+   } 
+   encoder3PinALast = p;
+   
+   p = digitalRead(val2);
+   if (p == LOW) {
+    encoder3Pos = 0;
    }
   
   
@@ -126,25 +126,24 @@ void loop() {
   
   int sensorValue1 = analogRead(A0);
   if (sensorValue1 != lastsensorValue) {
-  DMXSerial.write(dmxchannel(x), sensorValue1 >> 2);
+  DMXSerial.write(3, sensorValue1 >> 2);
   }
   lastsensorValue = sensorValue1;
   
   int sensorValue2 = analogRead(A1);
   if (sensorValue2 != lastsensorValue2) {
-  DMXSerial.write(dmxchannel(y), sensorValue2 >> 2);
+  DMXSerial.write(4, sensorValue2 >> 2);
   }
   lastsensorValue2 = sensorValue2;
   
-  // int sensorValue2 = analogRead(A1);
-  DMXSerial.write(dmxchannel(a), encoder0Pos);
   
-  DMXSerial.write(dmxchannel(b), encoder1Pos);
+  DMXSerial.write(1, encoder1Pos);
   
-  DMXSerial.write(dmxchannel(c), encoder2Pos);
+  DMXSerial.write(2, encoder2Pos);
+  
+  DMXSerial.write(5, encoder3Pos);
   
   
   
   
 } // loop
-
